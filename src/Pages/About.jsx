79eb872509ccef34299ed28025c92478179605fc
Lahiru Prasad <104630433+Lahiru-LK@users.noreply.github.com>
2025-05-13 21:21:25 +0530

    page about

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -214,10 +214,9 @@ const AboutPage = () => {
               data-aos="fade-right"
               data-aos-duration="1500"
             >
-              seorang siswa Teknik Jaringan Komputer dan Telekomunikasi yang
-              tertarik dalam pengembangan Front-End. Saya berfokus pada
-              menciptakan pengalaman digital yang menarik dan selalu berusaha
-              memberikan solusi terbaik dalam setiap proyek.
+              I'm a passionate Full Stack Developer and Software Engineering undergrad at the University of Ruhuna. 
+                I love building engaging and responsive web applications using React, Node.js, and modern tech stacks. 
+                Always eager to learn, solve problems, and bring ideas to life through clean code and creativity.
             </p>
 
             <div className="flex flex-col lg:flex-row items-center lg:items-start gap-4 lg:gap-4 lg:px-0 w-full">
